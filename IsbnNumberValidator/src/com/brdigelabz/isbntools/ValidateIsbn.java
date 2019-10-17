7a722c5435d9f1ba956a789a05dd82bc6d156ea8
AHETESHAMS <aheteshams007@gmail.com>
2019-10-17 14:40:38 +0530

    tested after refactoring

--- IsbnNumberValidator/src/com/brdigelabz/isbntools/ValidateIsbn.java
+++ IsbnNumberValidator/src/com/brdigelabz/isbntools/ValidateIsbn.java
@@ -2,19 +2,18 @@ package com.brdigelabz.isbntools;
 
 public class ValidateIsbn {
 
+	private static final int SHORT_ISBN_LENGTH = 10;
+	private static final int SHORT_ISBN_MULTIPLIER = 11;
+
 	public boolean checkIsbn(String isbnNumber) {
-		int total=0;
-		boolean result = false;
-		if(isbnNumber.length()!= 10) throw new NumberFormatException("Isbn Number must be 10 digits");
-		for(int i=0;i<10;i++)
+		int total = 0;
+		if(isbnNumber.length()!= SHORT_ISBN_LENGTH) throw new NumberFormatException("Isbn Number must be 10 digits");
+		for(int i=0;i<SHORT_ISBN_LENGTH;i++)
 		{
 			if(!Character.isDigit(isbnNumber.charAt(i))) throw new NumberFormatException("Only Digits are allowed");
-			total+=(isbnNumber.charAt(i)-48) * (10-i);
+			total+=(isbnNumber.charAt(i)-48) * (SHORT_ISBN_LENGTH-i);
 		}
-		if(total%11 == 0)
-			result = true;
-		
-		return result;
+		return(total % SHORT_ISBN_MULTIPLIER == 0);
 	}
 
 }
